60d152f09d70d349ca79b1b387bc83046737c50a
Ellis Brown <ellisbrown@cmu.edu>
2022-09-24 15:49:37 -0400

    fix discrete custom type

--- social_dilemmas/envs/gym/discrete_with_dtype.py
+++ social_dilemmas/envs/gym/discrete_with_dtype.py
@@ -1,9 +1,11 @@
+from typing import Optional
 from gym.spaces import Discrete
 
 
 class DiscreteWithDType(Discrete):
-    def __init__(self, n, dtype):
+    def __init__(self, n: int, dtype=int, seed: Optional[int] = None, start: int = 0):
         assert n >= 0
-        self.n = n
+        self.n = int(n)
+        self.start = int(start)
         # Skip Discrete __init__ on purpose, to avoid setting the wrong dtype
-        super(Discrete, self).__init__((), dtype)
+        super(Discrete, self).__init__((), dtype, seed)
