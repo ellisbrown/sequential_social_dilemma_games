a9aa2961aa9da629d3d59978dffa81f90084b39a
Tanmay Kulkarni <tanmaygk97@gmail.com>
2022-10-02 01:18:01 -0400

    some more precision hacks

--- models/moa_model.py
+++ models/moa_model.py
@@ -183,7 +183,6 @@ class MOAModel(RecurrentTFModelV2):
         prev_moa_trunk = input_dict["prev_moa_trunk"]
         other_actions = input_dict["other_agent_actions"]
         agent_action = tf.expand_dims(input_dict["prev_actions"], axis=-1)
-        agent_action = tf.cast(agent_action, tf.float32)
         all_actions = tf.concat([agent_action, other_actions], axis=-1, name="concat_true_actions")
         self._true_one_hot_actions = self._reshaped_one_hot_actions(all_actions, "forward_one_hot")
         true_action_pass_dict = {
