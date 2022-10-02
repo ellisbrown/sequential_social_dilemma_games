4f0ab11edb173fafdfb7d3d4c4ec5223f34c6941
Tanmay Kulkarni <tanmaygk97@gmail.com>
2022-10-01 23:49:15 -0400

    fix bugs in moa

--- algorithms/ppo_moa.py
+++ algorithms/ppo_moa.py
@@ -1,26 +1,21 @@
+from email.policy import default
 from ray.rllib.agents.ppo.ppo import (
     choose_policy_optimizer,
     update_kl,
     validate_config,
     warn_about_bad_reward_scales,
 )
-from ray.rllib.agents.ppo.ppo_tf_policy import (
-    KLCoeffMixin,
-    PPOLoss,
-    ValueNetworkMixin,
-    clip_gradients,
-    kl_and_loss_stats,
-    postprocess_ppo_gae,
-    setup_config,
-    vf_preds_fetches,
-)
-from ray.rllib.agents.trainer_template import build_trainer
+from ray.rllib.agents.ppo import PPOTrainer
+
+# Import PPOLoss from ray.rllib.agents.ppo.ppo_tf_policy.py
+
+from old_ppo_loss import PPOLoss
+from ray.rllib.agents.ppo import ppo_tf_policy
 from ray.rllib.evaluation.postprocessing import Postprocessing
 from ray.rllib.policy.sample_batch import SampleBatch
-from ray.rllib.policy.tf_policy import EntropyCoeffSchedule, LearningRateSchedule
+from ray.rllib.policy.tf_mixins import EntropyCoeffSchedule, LearningRateSchedule, KLCoeffMixin, ValueNetworkMixin
 from ray.rllib.policy.tf_policy_template import build_tf_policy
 from ray.rllib.utils import try_import_tf
-
 from algorithms.common_funcs_moa import (
     EXTRINSIC_REWARD,
     SOCIAL_INFLUENCE_REWARD,
@@ -38,7 +33,6 @@ tf1, tf, version = try_import_tf()
 
 POLICY_SCOPE = "func"
 
-
 def loss_with_moa(policy, model, dist_class, train_batch):
     """
     Calculate PPO loss with MOA loss
@@ -47,18 +41,17 @@ def loss_with_moa(policy, model, dist_class, train_batch):
     # you need to override this bit to pull out the right bits from train_batch
     logits, state = model.from_batch(train_batch)
     action_dist = dist_class(logits, model)
-
     moa_loss = setup_moa_loss(logits, policy, train_batch)
     policy.moa_loss = moa_loss.total_loss
-
+    
     if state:
         max_seq_len = tf.reduce_max(train_batch["seq_lens"])
         mask = tf.sequence_mask(train_batch["seq_lens"], max_seq_len)
         mask = tf.reshape(mask, [-1])
     else:
         mask = tf.ones_like(train_batch[Postprocessing.ADVANTAGES], dtype=tf.bool)
-
-    policy.loss_obj = PPOLoss(
+        
+        policy.loss_obj = PPOLoss(
         dist_class,
         model,
         train_batch[Postprocessing.VALUE_TARGETS],
@@ -77,27 +70,24 @@ def loss_with_moa(policy, model, dist_class, train_batch):
         policy.config["vf_loss_coeff"],
         policy.config["use_gae"],
     )
-
     policy.loss_obj.loss += moa_loss.total_loss
     return policy.loss_obj.loss
-
 
 def extra_moa_fetches(policy):
     """
     Adds value function, logits, moa predictions to experience train_batches.
     :return: Updated fetches
     """
-    ppo_fetches = vf_preds_fetches(policy)
+    ppo_fetches = ppo_tf_policy.vf_preds_fetches(policy)
     ppo_fetches.update(moa_fetches(policy))
     return ppo_fetches
-
 
 def extra_moa_stats(policy, train_batch):
     """
     Add stats that are logged in progress.csv
     :return: Combined PPO+MOA stats
     """
-    base_stats = kl_and_loss_stats(policy, train_batch)
+    base_stats = ppo_tf_policy.kl_and_loss_stats(policy, train_batch)
     base_stats = {
         **base_stats,
         "var_gnorm": tf.global_norm([x for x in policy.model.trainable_variables()]),
@@ -119,7 +109,7 @@ def postprocess_ppo_moa(policy, sample_batch, other_agent_batches=None, episode=
     :return: Updated trajectory (batch)
     """
     batch = moa_postprocess_trajectory(policy, sample_batch)
-    batch = postprocess_ppo_gae(policy, batch)
+    batch = ppo_tf_policy.postprocess_ppo_gae(policy, batch)
     return batch
 
 
@@ -133,7 +123,6 @@ def setup_ppo_moa_mixins(policy, obs_space, action_space, config):
     LearningRateSchedule.__init__(policy, config["lr"], config["lr_schedule"])
     setup_moa_mixins(policy, obs_space, action_space, config)
 
-
 def validate_ppo_moa_config(config):
     """
     Validates the PPO+MOA config
@@ -141,8 +130,7 @@ def validate_ppo_moa_config(config):
     """
     validate_moa_config(config)
     validate_config(config)
-
-
+    
 def build_ppo_moa_trainer(moa_config):
     """
     Creates a MOA+PPO policy class, then creates a trainer with this policy.
@@ -150,7 +138,6 @@ def build_ppo_moa_trainer(moa_config):
     :return: A new MOA+PPO trainer.
     """
     tf.keras.backend.set_floatx("float32")
-
     trainer_name = "MOAPPOTrainer"
 
     moa_ppo_policy = build_tf_policy(
@@ -161,14 +148,14 @@ def build_ppo_moa_trainer(moa_config):
         stats_fn=extra_moa_stats,
         extra_action_fetches_fn=extra_moa_fetches,
         postprocess_fn=postprocess_ppo_moa,
-        gradients_fn=clip_gradients,
-        before_init=setup_config,
+        gradients_fn=ppo_tf_policy.clip_gradients,
+        before_init=ppo_tf_policy.setup_config,
         before_loss_init=setup_ppo_moa_mixins,
         mixins=[LearningRateSchedule, EntropyCoeffSchedule, KLCoeffMixin, ValueNetworkMixin]
         + get_moa_mixins(),
     )
-
-    moa_ppo_trainer = build_trainer(
+    
+    moa_ppo_trainer = PPOTrainer.with_updates(
         name=trainer_name,
         default_policy=moa_ppo_policy,
         make_policy_optimizer=choose_policy_optimizer,
@@ -178,5 +165,5 @@ def build_ppo_moa_trainer(moa_config):
         after_train_result=warn_about_bad_reward_scales,
         mixins=[MOAResetConfigMixin],
     )
-
+    
     return moa_ppo_trainer
